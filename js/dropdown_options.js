844b6e2368e59c641969e375dbb6e68ed39caa06
Michael Machado <michael62machado@gmail.com>
2018-07-25 12:18:04 -0700

    Adding highlight on mouse over

---
+++ js/dropdown_options.js
@@ -0,0 +1,25 @@
+var dropdown_options = [{
+    value: "2013",
+    text: "2013 Incidents"
+  },
+  {
+    value: "2014",
+    text: "2014 Incidents"
+  },
+  {
+    value: "2015",
+    text: "2015 Incidents"
+  },
+  {
+    value: "2016",
+    text: "2016 Incidents"
+  },
+  {
+    value: "2017",
+    text: "2017 Incidents"
+  },
+  {
+    value: "2018",
+    text: "2018 Incidents"
+  },
+];
